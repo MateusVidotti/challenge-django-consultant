da5c799afb74e9e4e88bc4ac64f9ec65d527041a
mateu <mateus.vidotti@idgeo.com.br>
2023-09-13 16:59:46 -0300

    atualizando web

--- app/form/app.js
+++ app/form/app.js
@@ -4,6 +4,8 @@ const loadingElement = document.querySelector('#loading')
 const loadedElement = document.querySelector('#loaded')
 const loanForm = document.querySelector('.loan-form')
 const alert = document.querySelector('.alert');
+const modalPlace = document.querySelector(".modal-placeholder");
+
 
 window.addEventListener('load', function() {
     async function getFormFields(){
@@ -17,8 +19,7 @@ window.addEventListener('load', function() {
 
 // Post a loan request
 async function postLoanRequest(loanRequest){
-    const urlPost = "http://127.0.0.1:8000/api/loansrequest"
-    const response = await fetch(urlPost, {
+    const response = await fetch(url, {
         method: "POST",
         body: loanRequest,
         headers: {
@@ -32,12 +33,14 @@ async function postLoanRequest(loanRequest){
         }
         else{            
             displayAlert('solicitação realizada com sucesso', 'success');
+            addModalContainer();
         }
         });
     const data = await response.json()
 
 }
 
+// listeners
 loanForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let request = {};
@@ -46,20 +49,17 @@ loanForm.addEventListener('submit', (e) => {
         request[input.name] = input.value;
     });
     request = JSON.stringify(request);
-    postLoanRequest(request)
-    // .then(function(data){
-    //     if (!data.ok){
-    //         displayAlert('solicitação realizada com sucesso', 'danger');
-    //     }
-    //     else{
-    //         displayAlert('solicitação realizada com sucesso', 'success');
-    //     }
-    //     });     
+    postLoanRequest(request)  
     });
+
+// closeBtn.addEventListener("click", function () {
+//     removeModalContainer();
+//     });
 
 
 // display alert
 function displayAlert(text, action){
+    //show alert
     alert.textContent = text;
     alert.classList.add(`alert-${action}`);
     
@@ -67,6 +67,24 @@ function displayAlert(text, action){
     setTimeout(function(){
         alert.textContent = '';
         alert.classList.remove(`alert-${action}`);
-    }, 1000);
+    }, 5000);
+}
 
+// modal container
+function addModalContainer(){
+    modalPlace.classList.add('modal-overlay');
+    modalPlace.innerHTML = `<div class="modal-container">
+    <p>Solicitação realizada com sucesso. Em breve</p>
+    <button class="close-btn"><i class="fas fa-times"></i></button>
+  </div>`;
+    const closeBtn = document.querySelector(".close-btn");
+    closeBtn.addEventListener("click", function () {
+        removeModalContainer();
+    });
+    loanForm.reset()
+}
+
+function removeModalContainer(){
+    modalPlace.classList.remove('modal-overlay');
+    modalPlace.innerHTML = ``
 }
